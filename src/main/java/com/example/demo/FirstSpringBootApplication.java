a538ee3baac203c7c2205326b06271899205f49c
youngcris <youngcris802@naver.com>
2020-09-23 14:22:22 +0900

    First Spring Boot init

---
+++ src/main/java/com/example/demo/FirstSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplication.class, args);
+		System.out.println("First Spring Boot Appliction~");
+	}
+
+}
